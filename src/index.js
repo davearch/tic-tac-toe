46d3d5eaf11019e3ad55c18133f884fb87b4472b
David Archuleta <darchuletajr@gmail.com>
2019-10-20 22:30:07 -0700

    added onClick method to board component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { throwError } from 'rxjs';
 
 class Square extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          value: null,
-      };
-  }
   render() {
     return (
       <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -28,8 +23,18 @@ class Board extends React.Component {
           squares: Array(9).fill(null),
       };
   }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
