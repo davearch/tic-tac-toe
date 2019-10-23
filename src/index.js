b17334af0e9ca97b7eb45aec892f3acd95b1b374
David Archuleta <darchuletajr@gmail.com>
2019-10-23 15:55:50 -0700

    take turns between x and o

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { throwError } from 'rxjs';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-          squares: Array(9).fill(null),
+        squares: Array(9).fill(null),
+        xIsNext: true,
       };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      xIsNext : !this.state.xIsNext,
+      squares: squares
+    });
   }
   renderSquare(i) {
     return (
@@ -38,7 +36,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
